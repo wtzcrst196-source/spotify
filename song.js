e475e8cafe2eedaeda0c5df478ee7a26d9b057b2
witz <wtzcrst196@gmail.com>
2026-03-10 19:48:48 -0400

    Update song.js

--- song.js
+++ song.js
@@ -2,25 +2,25 @@ const songs = [
     {
         title: "Peaches",
         artist: "Echo System",
-        src: "music/song1.mp3",
+        audio: "music/song1.mp3",
         cover: "images/image1.jpg"
     },
     {
         title: "Consume",
         artist: "Neon Sky",
-        src: "music/song2.mp3",
+        audio: "music/song2.mp3",
         cover: "images/image2.jpg"
     },
     {
         title: "Intension",
         artist: "Horizon",
-        src: "music/song3.mp3",
+        audio: "music/song3.mp3",
         cover: "images/image3.jpg"
     },
     {
         title: "Next Door",
         artist: "Stellar",
-        src: "music/song4.mp3",
+        audio: "music/song4.mp3",
         cover: "images/1.jpg"
     }
 ];
