0dfb0ea11598bf4aee29adee5a39a7b91d87e95a
witz <wtzcrst196@gmail.com>
2026-03-10 19:59:26 -0400

    Update song.js

--- song.js
+++ song.js
@@ -1,26 +1,20 @@
 const songs = [
-    {
-        title: "Peaches",
-        artist: "Justin Bieber",
-        audio: "music/song1.mp3",
-        cover: "images/image1.jpg"
-    },
-    {
-        title: "Consume",
-        artist: "Chase Atlantic",
-        audio: "music/song2.mp3",
-        cover: "images/image2.jpg"
-    },
-    {
-        title: "Intension",
-        artist: "Horizon",
-        audio: "music/song3.mp3",
-        cover: "images/image3.jpg"
-    },
-    {
-        title: "Next Door",
-        artist: "Stellar",
-        audio: "music/song4.mp3",
-        cover: "images/1.jpeg"
-    }
-];
+      {
+        title: 'Peaches',
+        artist: 'Justin Bieber',
+        cover: 'images/1.jpeg',
+        audio: 'music/song1.mp3'
+      },
+      {
+        title: 'Neon Horizon',
+        artist: 'Synthwave Drifter',
+        cover: 'images/image2.jpg',
+        audio: 'music/song2.mp3'
+      },
+      {
+        title: 'Deep Echoes',
+        artist: 'Submersible',
+        cover: 'images/image3.jpg',
+        audio: 'music/song3.mp3'
+      }
+    ];
