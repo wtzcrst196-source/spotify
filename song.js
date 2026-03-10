86ff2ecc1432f7dce4222c412976eec0d7a05019
witz <wtzcrst196@gmail.com>
2026-03-10 18:39:37 -0400

    Create song.js

---
+++ song.js
@@ -0,0 +1,18 @@
+const songs=[
+{name:"Peaches",
+ src:"music/song1.mp3",
+ artist:'Justin Bieber',
+ image:'images/IMG_0892.jpeg'},
+{name:"Consume",
+ src:"music/song2.mp3",
+ artist:'Chase Atlantic',
+ image:'images/IMG_0893.jpeg'},
+{name:"Intention",
+ src:"music/song3.mp3",
+ artist:'Justin Bieber & Quavo',
+ image:'images/IMG_0894.jpeg'},
+{name:"Next door",
+ src:"music/song4.mp3",
+ artist:'ASTN & Amelia Moore',
+ image:'images/1.jpeg'}
+]
